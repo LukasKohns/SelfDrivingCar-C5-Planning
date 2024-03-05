d1aa2662045fa75271b311693b7acac60e729152
Kohns Lukas (XC-AD/EPE6) <Lukas.Kohns@de.bosch.com>
2024-03-05 14:28:19 +0100

    finish TODOs in velocity_profile_generator

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -357,12 +357,13 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
   if (std::abs(a) < DBL_EPSILON) {
     d = std::numeric_limits<double>::infinity();
   } else {
-    // TODO-calc distance: use one of the common rectilinear accelerated
+    // DONE -calc distance: use one of the common rectilinear accelerated
     // equations of motion to calculate the distance traveled while going from
     // v_i (initial velocity) to v_f (final velocity) at a constant
     // acceleration/deceleration "a". HINT look at the description of this
-    // function. Make sure you handle div by 0
-    d = 0;  // <- Update
+    // function. 
+    // Make sure you handle div by 0 -> already handled above so no idea what this comment means
+    d = (std::pow(v_f, 2) - std::pow(v_i, 2)) / (2 * a);  // formula given in comment
   }
   return d;
 }
@@ -380,12 +381,12 @@ double VelocityProfileGenerator::calc_final_speed(const double& v_i,
                                                   const double& a,
                                                   const double& d) const {
   double v_f{0.0};
-  // TODO-calc final speed: Calculate the final distance. HINT: look at the
+  // DONE-calc final speed: Calculate the final distance. HINT: look at the
   // description of this function. Make sure you handle negative discriminant
   // and make v_f = 0 in that case. If the discriminant is inf or nan return
   // infinity
 
-  double disc = 0;  // <- Fix this
+  double disc = std::pow(v_i, 2) + 2 * a * d;  // <- Given above
   if (disc <= 0.0) {
     v_f = 0.0;
   } else if (disc == std::numeric_limits<double>::infinity() ||
